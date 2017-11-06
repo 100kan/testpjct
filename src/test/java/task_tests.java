56182324bd8f2d6f718f0b3f504e172e0dcf1d60
Kolian Mishutkin <Kolian.mishutkin@gmail.com>
2017-11-06 21:53:26 +0300

    initial commit

---
+++ src/test/java/task_tests.java
@@ -0,0 +1,86 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class task_tests {
+
+
+@Test
+
+
+public void test1(){
+System.out.println("Test 1 started");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://www.wiley.com/WileyCDA/");
+
+
+    Assert.assertNotNull(driver.findElement(By.id("links-site")).findElements(By.linkText("Home")));
+    Assert.assertNotNull(driver.findElement(By.id("links-site")).findElements(By.linkText("Subjects")));
+    Assert.assertNotNull(driver.findElement(By.id("links-site")).findElements(By.linkText("About Wiley")));
+    Assert.assertNotNull(driver.findElement(By.id("links-site")).findElements(By.linkText("Contact Us")));
+    Assert.assertNotNull(driver.findElement(By.id("links-site")).findElements(By.linkText("Help")));
+
+driver.quit();
+}
+
+@Test
+public void test2() {
+    System.out.println("Test 2 started");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://www.wiley.com/WileyCDA/");
+
+
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Students")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Authors")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Instructors")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Librarians")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Societies")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Conferences")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Booksellers")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Corporations")));
+    Assert.assertNotNull(driver.findElement(By.id("homepage-links")).findElement(By.linkText("Institutions")));
+
+    Assert.assertTrue(driver.findElement(By.id("homepage-links")).findElements(By.tagName("a")).size() == 9);
+
+    driver.quit();
+}
+
+
+
+    @Test
+    public void test3() {
+        System.out.println("Test 3 started");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.wiley.com/WileyCDA/");
+
+driver.findElement(By.linkText("Students")).click();
+
+
+        WebElement element;
+        element = driver.findElement(By.cssSelector("div #page-title h1"));
+Assert.assertEquals(element.getText(),"Students");
+Assert.assertEquals(driver.getCurrentUrl(), "http://www.wiley.com/WileyCDA/Section/id-404702.html "); // тест крашится для всех регионов кроме Americas.
+        driver.quit();
+
+
+    }
+    @Test
+    public void test4() {
+        System.out.println("Test 4 started");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.wiley.com/WileyCDA/");
+
+        driver.findElement(By.linkText("Students")).click();
+
+
+
+
+
+
+    }
+
+
+}
